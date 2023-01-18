10660002384442f73f4457313335a7956b824db2
vankrav <vankraf@ya.ru>
2023-01-18 23:20:06 +0300

    добавил первый тест

---
+++ Test/tst_test_tetris.cpp
@@ -0,0 +1,55 @@
+#include <QtTest>
+#include "../tetris.h"
+// add necessary includes here
+
+class Test_Tetris : public QObject
+{
+    Q_OBJECT
+
+public:
+    Test_Tetris();
+    Tetris t;
+
+private slots:
+    void bestScore();
+    void move();
+
+
+
+};
+
+Test_Tetris::Test_Tetris()
+{
+
+}
+
+
+void Test_Tetris::bestScore()
+{
+    Tetris t;
+
+    QCOMPARE(t.bestScore(10), 10);
+    QCOMPARE(t.bestScore(11), 11);
+    QCOMPARE(t.bestScore(9), 11);
+    QCOMPARE(t.bestScore(6), 11);
+
+}
+
+void Test_Tetris::move()
+{
+
+    Tetris t;
+    int x = t.getBlock().centerX;
+    int y = t.getBlock().centerY;
+
+    t.moveToBottom();
+    QCOMPARE(x, (MAXX - 1) / 2);
+
+
+
+
+}
+
+QTEST_APPLESS_MAIN(Test_Tetris)
+
+#include "tst_test_tetris.moc"
